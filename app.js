9f56f7a485f07b7cdaa59854eb2a4aeaa0738c8b
Veeresh <veereshrm789@gmail.com>
2021-11-20 11:28:11 +0530

    Innitial Commit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ let windows = document.getElementsByClassName('windows')[0]
 let startmenu = document.getElementsByClassName('startmenu')[0]
 
 windows.addEventListener("click",()=>{
-    if(startmenu.style.bottom == '30px'){
+    if(startmenu.style.bottom == '40px'){
         startmenu.style.bottom = '-400px';
     }
     else{
-        startmenu.style.bottom = '30px';
+        startmenu.style.bottom = '40px';
     }
     
 })
